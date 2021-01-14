964e91d1c7cb9da2fd31ef22f22bb1172e137361
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-01-14 17:29:27 +0700

    init

---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ info }}</h1>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+export default {
+  name: "List",
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    Vue.axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(response => {
+        console.warn((this.info = response.data.bpi));
+      });
+  }
+};
+</script>
